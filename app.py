8fbb43d273c32da2014ba20c35a1aeb031381876
Naman Khurana <namankhurana2017@gmail.com>
2026-02-22 13:15:58 +0530

    basic project setup

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from quizmastercode import create_app
 
-def create_app():
-    app=Flask(__name__)
+
+app=create_app()
+
+if(__name__=="__main__"):
+    app.run(debug=True)
